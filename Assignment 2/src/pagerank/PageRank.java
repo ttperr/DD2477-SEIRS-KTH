43378c5e5f111ace6bbba476a126edb80243b058
Tristan Perrot <tt.perrot@gmail.com>
2024-02-07 20:04:59 +0100

    task 2.5 wip

--- Assignment 2/src/pagerank/PageRank.java
+++ Assignment 2/src/pagerank/PageRank.java
@@ -132,15 +132,103 @@ public class PageRank {
      */
     void iterate(int numberOfDocs, int maxIterations) {
         // YOUR CODE HERE
-        double[] scores = new double[numberOfDocs];
+        System.err.println("Started Iteration...");
+        double startTime = System.currentTimeMillis();
+        double[] scores;
         double[] newScores = new double[numberOfDocs];
-        newScores[0] = 1;
-        for (int i = 0; i < numberOfDocs; i++) {
-            scores = newScores;
-            // TODO: Multiply with the transition matrix
+        newScores[0] = 1.0;
+        double[] inverseOut = new double[numberOfDocs];
+        for (int k = 0; k < numberOfDocs; k++) {
+            inverseOut[k] = 1.0 / out[k];
         }
+
+
+        for (int i = 0; i < maxIterations; i++) {
+            scores = newScores.clone();
+            for (int j = 0; j < numberOfDocs; j++) {
+                newScores[j] = 0;
+                for (int k = 0; k < numberOfDocs; k++) {
+                    if (link.get(k) != null && link.get(k).get(j) != null) {
+                        newScores[j] += scores[k] * inverseOut[k];
+                    }
+                }
+                newScores[j] = BORED / numberOfDocs + (1 - BORED) * newScores[j];
+            }
+            boolean isConverged = convergenceCriteria(scores, newScores);
+            System.out.println("Iteration " + i);
+            if (isConverged) {
+                break;
+            }
+            if (System.currentTimeMillis() - startTime > 180000) {
+                System.err.println("Time limit reached");
+                break;
+            }
+        }
+        printPageRank(newScores, true);
+        double estimatedTime = (System.currentTimeMillis() - startTime) / 1000.0;
+        System.err.println("Done! Time: " + estimatedTime + "s");
     }
 
+    private boolean convergenceCriteria(double[] tab1, double[] tab2) {
+        for (int i = 0; i < tab1.length; i++) {
+            if (Math.abs(tab1[i] - tab2[i]) > EPSILON) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void printPageRank(double[] scores, boolean top30) {
+        int numberOfDocs = scores.length;
+        // Insert into davis_top_30_test.txt the 30 most important pages
+        List<Integer> resultList = new ArrayList<>();
+        if (top30) {
+            for (int i = 0; i < 30; i++) {
+                double max = 0;
+                int maxIndex = 0;
+                for (int j = 0; j < numberOfDocs; j++) {
+                    if (scores[j] > max && !resultList.contains(j)) {
+                        max = scores[j];
+                        maxIndex = j;
+                    }
+                }
+                resultList.add(maxIndex);
+            }
+        } else {
+            // Insert all into resultList
+            for (int i = 0; i < numberOfDocs; i++) {
+                resultList.add(i);
+            }
+        }
+        // Write in the file
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("src/pagerank/davis_top_30_test.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("src/pagerank/davisTitles.txt"));
+            String line;
+            int docID;
+            String[] docTitle = new String[resultList.size()];
+            while ((line = reader.readLine()) != null) {
+                docID = Integer.parseInt(line.split(";")[0]);
+                int indexOfDocID = -1;
+                for (int i = 0; i < docName.length; i++) {
+                    if (Integer.parseInt(docName[i]) == docID) {
+                        indexOfDocID = i;
+                        break;
+                    }
+                }
+                int j = resultList.indexOf(indexOfDocID);
+                if (j != -1) {
+                    docTitle[j] = line.split(";")[1];
+                }
+            }
+            for (int i = 0; i < 30; i++) {
+                writer.write(docName[resultList.get(i)] + ": " + String.format("%.5f", scores[resultList.get(i)]) + ": " + docTitle[i] + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /* --------------------------------------------- */
 
